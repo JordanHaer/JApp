1f9bcd305d30833d501fcb8ad63cbaf9bd61f26e
JordanHaer <jordanhaer@gmail.com>
2024-11-12 20:53:46 +0000

    Initial Commit

---
+++ JApp/JAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JAppApp.swift
+//  JApp
+//
+//  Created by Jordan Haer on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct JAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
